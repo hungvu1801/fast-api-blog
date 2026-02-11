6487cbbe2d47ffd97b33338416e7adc5fa1776ad
NguyenThuyComputer <vutienhung2212@gmail.com>
2026-02-11 23:50:52 +0700

    add files in auth

---
+++ auth.py
@@ -0,0 +1,19 @@
+from datetime import UTC, datetime, timedelta
+
+import jwt
+from fastapi.security import OAuth2PasswordBearer
+from pwdlib import PasswordHash
+
+from config import settings
+
+password_hash = PasswordHash.recommended()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/users/token")
+
+
+def hashpassword(password: str) -> str:
+    return password_hash.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return password_hash.verify(plain_password, hashed_password)
