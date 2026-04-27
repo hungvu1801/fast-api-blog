85153eb55dad56d80afe723ae07b350da398797b
NguyenThuyComputer <vutienhung2212@gmail.com>
2026-04-27 23:54:33 +0700

    [feat]: Reset password cont.

--- auth.py
+++ auth.py
@@ -31,7 +31,7 @@ def generate_reset_token() -> str:
     return secrets.token_urlsafe(32)
 
 
-def hash_reset_toekn(token: str) -> str:
+def hash_reset_token(token: str) -> str:
     return hashlib.sha256(token.encode()).hexdigest()
 
 
